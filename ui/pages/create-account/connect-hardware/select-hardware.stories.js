531ed456e8481e4fdda973ba356bad1ea2f28396
alekshoxhaer4786 <146690878+alekshoxhaer4786@users.noreply.github.com>
2025-07-07 19:33:03 -0600

    Create select-hardware.stories.js

---
+++ ui/pages/create-account/connect-hardware/select-hardware.stories.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import { LedgerTransportTypes } from '../../../../shared/constants/hardware-wallets';
+import SelectHardware from './select-hardware';
+
+export default {
+  title: 'Pages/CreateAccount/ConnectHardware/SelectHardware',
+};
+
+export const DefaultStory = () => (
+  <SelectHardware
+    onCancel={() => null}
+    browserSupported
+    connectToHardwareWallet={(selectedDevice) =>
+      action(`Continue connect to ${selectedDevice}`)()
+    }
+    ledgerTransportType={LedgerTransportTypes.live}
+  />
+);
+
+DefaultStory.storyName = 'Default';
+
+export const BrowserNotSupported = () => (
+  <SelectHardware
+    onCancel={() => null}
+    browserSupported={false}
+    connectToHardwareWallet={() => undefined}
+    ledgerTransportType={LedgerTransportTypes.live}
+  />
+);
