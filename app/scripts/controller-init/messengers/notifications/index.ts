aad22cc933175d4b14b4c0cafa7c4ae8cb652c53
alekshoxhaer4786 <146690878+alekshoxhaer4786@users.noreply.github.com>
2025-07-24 01:59:09 -0600

    Create index.ts

---
+++ app/scripts/controller-init/messengers/notifications/index.ts
@@ -0,0 +1,2 @@
+export { getNotificationServicesControllerMessenger, type NotificationServicesControllerMessenger } from './notification-services-controller-messenger';
+export { getNotificationServicesPushControllerMessenger, type NotificationServicesPushControllerMessenger } from './notification-services-push-controller-messenger';
